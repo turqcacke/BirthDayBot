c8c474b4a43dfc0dd6d96b0b7bb67f21cedaa308
Turquare <60967566+turqcacke@users.noreply.github.com>
2021-07-14 04:23:17 +0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pytz
-from datetime import datetime
-
-if __name__ == '__main__':
-    print(pytz.timezone('Asia/Tashkent'))
-    print(datetime.now().date())
-    print(datetime.now(tz=pytz.utc).date())
-    print(datetime.now(tz=pytz.utc).date() < datetime.now().date())
